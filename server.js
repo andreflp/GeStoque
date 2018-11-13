a1c1254d2f372f1e3f556d0390c46459203b3ef3
andre <flpandresilva@gmail.com>
2018-11-13 17:56:12 -0200

    Server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const port = process.env.PORT;
+const port = process.env.PORT || 8081;
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function (req, res) {
   res.sendfile(__dirname + "/dist/index.html")
 });
-app.listen(port);
+server.listen(port, function () {
+  console.log("App is running on port " + port);
+});
 
 console.log("Server started...");
